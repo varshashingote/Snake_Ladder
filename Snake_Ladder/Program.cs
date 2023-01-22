5c788d331bae74ac41493bff0017d5230fe6fe71
varsha Gunjal <varshashingote01@gmail.com>
2023-01-22 15:55:45 +0530

    [varsha] welcome message

---
+++ Snake_Ladder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder");
+            Console.ReadLine();
+        }
+    }
+}
