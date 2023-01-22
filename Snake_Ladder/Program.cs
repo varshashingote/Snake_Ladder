7a60673caabf16e39d36bc46cf0774980d600f8f
varsha Gunjal <varshashingote01@gmail.com>
2023-01-22 16:16:14 +0530

    [varsha] option

--- Snake_Ladder/Program.cs
+++ Snake_Ladder/Program.cs
@@ -11,16 +11,31 @@ namespace Snake_Ladder
     {
         static void Main(string[] args)
         {
+            int PlayerPosition = 0;
             int start = 0;
             Console.WriteLine("Start Position:" + start);
            
             Random random = new Random();
             int roll = random.Next(0, 7);
+            Console.WriteLine("After Rolling a dice"+roll);
+            int Option = random.Next(0, 3);
+            Console.ReadLine(); 
+            if (Option == 0)
+            {
+                PlayerPosition = PlayerPosition - roll;
+                Console.WriteLine("snake " + PlayerPosition);
+            }
+            else if (Option == 1)
+            {
+                PlayerPosition = PlayerPosition + roll;
+                Console.WriteLine("ladder " + PlayerPosition);
+            }
+            else
+            {
+                Console.WriteLine("no play " + PlayerPosition);
+            }
             Console.ReadLine();
-
-
         }
-
-    }
     }
 }
+
