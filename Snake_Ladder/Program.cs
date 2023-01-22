13c7a72e546544b1049ccb60db7a86269b80b2a0
varsha Gunjal <varshashingote01@gmail.com>
2023-01-22 16:21:00 +0530

    [varsha] Rechaces wining

--- Snake_Ladder/Program.cs
+++ Snake_Ladder/Program.cs
@@ -14,27 +14,30 @@ namespace Snake_Ladder
             int PlayerPosition = 0;
             int start = 0;
             Console.WriteLine("Start Position:" + start);
-           
-            Random random = new Random();
-            int roll = random.Next(0, 7);
-            Console.WriteLine("After Rolling a dice"+roll);
-            int Option = random.Next(0, 3);
-            Console.ReadLine(); 
-            if (Option == 0)
+            while (PlayerPosition < 100)
             {
-                PlayerPosition = PlayerPosition - roll;
-                Console.WriteLine("snake " + PlayerPosition);
+                Random random = new Random();
+                int roll = random.Next(0, 7);
+                Console.WriteLine("After Rolling a dice" + roll);
+                int Option = random.Next(0, 3);
+                Console.WriteLine("Option is " + Option);
+                Console.ReadLine();
+                if (Option == 0)
+                {
+                    PlayerPosition = PlayerPosition - roll;
+                    Console.WriteLine("snake " + PlayerPosition);
+                }
+                else if (Option == 1)
+                {
+                    PlayerPosition = PlayerPosition + roll;
+                    Console.WriteLine("ladder " + PlayerPosition);
+                }
+                else
+                {
+                    Console.WriteLine("no play " + PlayerPosition);
+                }
+                Console.ReadLine();
             }
-            else if (Option == 1)
-            {
-                PlayerPosition = PlayerPosition + roll;
-                Console.WriteLine("ladder " + PlayerPosition);
-            }
-            else
-            {
-                Console.WriteLine("no play " + PlayerPosition);
-            }
-            Console.ReadLine();
         }
     }
 }
